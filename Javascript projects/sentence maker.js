4039d3095139ea6327ebe89e841d415378a470d4
EEdoolotu <131226967+EEdoolotu@users.noreply.github.com>
2025-09-23 14:45:49 +0100

    sent

---
+++ Javascript projects/sentence maker.js
@@ -0,0 +1,22 @@
+let adjective = "terrible";
+let noun = "dragon";
+let verb = "angry";
+let place = "Nigeria";
+let adjective2 = "huge";
+let noun2 = "animals";
+
+let firstStory;
+firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+console.log("First story: " + firstStory)
+
+adjective = "huge ";
+noun = "lion ";
+verb = "hunting";
+place = "Africa";
+adjective2 = "small";
+noun2 = "gazelles";
+
+let secondStory;
+secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+
+console.log("Second story: " + secondStory)
