536ea14e0e61baeabcb33e46a7f83cfd9a3e41f7
EEdoolotu <131226967+EEdoolotu@users.noreply.github.com>
2025-09-23 13:51:11 +0100

    trivia

---
+++ Javascript projects/Trivia Bot.js
@@ -0,0 +1,21 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "Nigerianbot";
+let botLocation = "Southampton";
+let favoriteLanguage = "English";
+
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".")
+console.log("My favorite programming language is " + favoriteLanguage + ".")
+
+let codingFact;
+codingFact = "The fact python is in " + favoriteLanguage + " is great";
+console.log(codingFact);
+
+codingFact = "Interestingly, " + favoriteLanguage + " is my preferred toungue in everything";
+
+console.log(codingFact);
+codingFact = "Interestingly, " + (favoriteLanguage) + " is the preferred toungue in the U.K";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
