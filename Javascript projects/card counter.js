feb5f482f9ca750eec814ea8c42c7441c8a38779
EEdoolotu <ejirogheneedoolotu@gmail.com>
2025-09-25 04:30:09 +0100

    cardfixed

--- Javascript projects/card counter.js
+++ Javascript projects/card counter.js
@@ -1,17 +1,16 @@
 let count = 0;
 
 function cc(card) {
-  if (card >= 2 || card <=6) {
-    count++ ;
-  } else if ( card >= 7 || card <=9) {
-    return count;
-  }else if (card == 10, "J", "Q", "K", "A") {
+  if (card >= 2 && card <=6) {
+    count++;
+  } else if ( card >= 7 && card <=9) {
+    count += 0;
+  } else {
     count--;
-  }else if (count > 0) {
+  } 
+  if (count > 0) {
     return count + " Bet";
-  } else if (count <= 0) {
+  } else {
     return count + " Hold";
-
+  }
 }
-
-console.log(cc(9))
