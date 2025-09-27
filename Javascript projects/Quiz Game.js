2053760d63e506ae3c25de62f621b1b256d74f1c
EEdoolotu <ejirogheneedoolotu@gmail.com>
2025-09-27 14:30:47 +0100

    quiz

---
+++ Javascript projects/Quiz Game.js
@@ -0,0 +1,38 @@
+const questions = [
+  {category: "urology",
+  question: "How many kidneys do humans have?",
+  choices: ["three", "six", "two"],
+  answer: "two"}, 
+  { 
+  category: "cardiology",
+  question: "What is the main function of the heart?",
+  choices: ["pump blood", "digest food", "filter toxins"],
+  answer: "pump blood"
+},
+
+{ 
+  category: "neurology",
+  question: "Which part of the brain controls balance and coordination?",
+  choices: ["cerebellum", "frontal lobe", "hippocampus"],
+  answer: "cerebellum"
+},
+
+{ 
+  category: "orthopedics",
+  question: "Which is the largest bone in the human body?",
+  choices: ["femur", "tibia", "humerus"],
+  answer: "femur"
+},
+
+{ 
+  category: "dermatology",
+  question: "What is the outermost layer of the skin called?",
+  choices: ["dermis", "epidermis", "hypodermis"],
+  answer: "epidermis"
+}
+
+]
+
+function getRandomQuestion(questions) {
+  
+}
