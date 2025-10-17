8bcbf6e5fc8df6c1fe5b9c5e3665646acfd5b2ac
EEdoolotu <ejirogheneedoolotu@gmail.com>
2025-10-17 22:16:47 +0100

    rea

--- REACT/index.jsx
+++ REACT/index.jsx
@@ -1,5 +1,9 @@
 import {createRoot} from "react-dom/client"
 
-const root = createRoot(document.getElementById("root"))
-
-root.render(<h1>Hello React!</h1>)
+createRoot(document.getElementById("root").render(
+    <ul>
+        <li>iT IS A PRETTY DOPE LIBRARY</li>
+        <li>I love the fact that it can do anything i want without the hassle of having to master 3 languages</li>
+        <li>It is recommended</li>
+    </ul>
+))
